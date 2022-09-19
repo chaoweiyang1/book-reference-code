aa74560c4c12ac5012f2e1213736145d81f9a196
chaoweiyang1 <108542697+chaoweiyang1@users.noreply.github.com>
2022-09-19 16:27:57 -0400

    Update code 4.8.py

--- 4/code 4.8.py
+++ 4/code 4.8.py
@@ -1,7 +1,7 @@
 >>> bIntersect = False
 >>> def intersect(line1,line2):
-        for i in len(line1.lineSegments):
-            for j in len(line2.lineSegments):
+        for i in range(len(line1.lineSegments)):
+            for j in range(len(line2.lineSegments)):
                 if (line1.lineSegments[i].segIntersect(line2.lineSegments[j])):
                     bIntersect = True
                     break
