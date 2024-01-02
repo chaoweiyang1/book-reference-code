bb72a6b6d3ee4b46edfe720d7fee059951fa38f2
Snizze <lypank99@gmail.com>
2024-01-02 15:20:17 -0500

    Comment on chapter 4.15

--- 4/run 4.15.py
+++ 4/run 4.15.py
@@ -1,5 +1,5 @@
-f = open ('points.txt', 'w+')
-f.write('point:\n')
+f = open ('points.txt', 'w+') # Change path as needed
+f.write('point:\n') # Change path as needed
 f.write('p1: 1.0, 1.0 \n')
 f.write('p2: 2.0, 2.0 \n')
 f.close()
